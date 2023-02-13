2a1a2f27e2b7d4a32c1b042d56c3b8f5f3226f0a
R3sty <iral.resty@gmail.com>
2023-02-13 10:41:05 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+<h1 className="text-pink-500">App works!</h1>
+    </div>
+  )
+}
+
+export default App
