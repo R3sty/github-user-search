bad0de3cda1a71283424f2e3d690a6f77b944d35
R3sty <iral.resty@gmail.com>
2023-02-15 13:47:50 +0900

    set mobile to full screen

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   };
 
   return (
-    <div className="bg-lightGrey dark:bg-blackDm h-full w-full desktop:w-full desktop:h-full">
+    <div className="bg-lightGrey dark:bg-blackDm h-screen w-screen desktop:w-full desktop:h-full">
       <div className="font-spaceMono tablet:w-[573px] tablet:h-screen tablet:mx-auto tablet:flex tablet:flex-col tablet:align-middle tablet:justify-center desktop:flex desktop:flex-col desktop:align-middle desktop:justify-center desktop:w-[730px] desktop:mx-auto">
         <Nav handleThemeSwitch={handleThemeSwitch} theme={theme}  />
       <Search handleSetSearchData={handleSetSearchData} />
